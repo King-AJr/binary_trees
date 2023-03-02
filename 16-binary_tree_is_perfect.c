489c36a1fb197fc565049ec2d2c651c804bc52f7
King-AJr <talk2king.aj@gmail.com>
2023-03-02 15:38:50 -0800

    add height function to file 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,16 @@
 #include "binary_trees.h"
+
+/**                                                    * tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *                                                     * Return: If tree is NULL, your function must return 0, else return height.
+ */                                                   size_t tree_height(const binary_tree_t *tree)  {                                                             if (tree)                                             {
+                size_t l = 0, r = 0;                  
+                l = tree->left ? 1 + tree_height(tree->left) : 0;
+                r = tree->right ? 1 + tree_height(tree->right) : 0;
+                return ((l > r) ? l : r);
+        }                                                     return (0);
+}
+
 /**
 * binary_tree_is_perfect - check if a binary tree is perfect
 *
@@ -13,8 +25,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (1);
 	}
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	size_t left_height = tree_height(tree->left);
+	size_t right_height = tree_height(tree->right);
 
 	if (left_height != right_height)
 	{
